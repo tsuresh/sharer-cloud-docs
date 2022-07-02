43c83876555a9510ddf53fe3845a7abe349fc14f
Suresh Peiris <tsmpmail@gmail.com>
2022-07-02 13:05:14 +0530

    Changed copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2022, Inforwaves Private Limited'
+author = 'Suresh Peiris'
 
 release = '0.1'
 version = '0.1.0'
